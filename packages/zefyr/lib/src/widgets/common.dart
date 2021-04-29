1c437b20c06aed61b7aa01fabe4f4cfffcb384c9
Shivam Kaushik <shivam@patratlabs.com>
2021-04-29 14:40:37 +0530

    common:158 embed null handler

--- packages/zefyr/lib/src/widgets/common.dart
+++ packages/zefyr/lib/src/widgets/common.dart
@@ -155,6 +155,7 @@ class _ZefyrLineState extends State<ZefyrLine> {
     EmbedNode node = widget.node.children.single;
     EmbedAttribute embed = node.style.get(NotusAttribute.embed);
 
+    embed ??= EmbedAttribute.horizontalRule();
     if (embed.type == EmbedType.horizontalRule) {
       return ZefyrHorizontalRule(node: node);
     } else if (embed.type == EmbedType.image) {
